17fe931c2cd40e071fc50a250df7fadac86e6bc3
StepArthur <snr.arthur@gmail.com>
2023-06-19 15:50:58 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print all numbers in base 10
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int y;
+
+	for (y = 0; y < 10; y++)
+	putchar(y + '0');
+	putchar('\n');
+	return (0);
+
+
+}
