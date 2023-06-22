f6330671da2d6ab3b764520a6064645aec7c7ed8
StepArthur <snr.arthur@gmail.com>
2023-06-22 07:28:49 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - Check if a character is a digit
+ * @a: The number to be checked
+ * Return: 1 for a character that is a digit, 0 for any else
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+        for (a = 0; a <= 9; a++)
+        {
+        _putchar(a + '0');
+        }
+        _putchar('\n');
+
+
+
+}
