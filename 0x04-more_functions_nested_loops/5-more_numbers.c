11acdbd3e53f3377800b23c37324607b7ff95b95
StepArthur <snr.arthur@gmail.com>
2023-06-22 08:39:58 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+
+#include "main.h"
+/**
+ * _isdigit - Check if a character is a digit
+ * @a: The number to be checked
+ * Return: 1 for a character that is a digit, 0 for any else
+ */
+
+void more_numbers(void)
+{
+
+        int x, y;
+        for (x = 0; x < 10; x++)
+        {
+        for (y = 0; y <= 14; y++)
+        {
+        if (y > 9)
+        {
+        _putchar(y / 10) + '0');
+        }
+	_putchar(y % 10) + '0');
+        }
+        _putchar('\n');
+        }
+
+
+
+}
+~                                                                                                                                                                                                            
+~             
